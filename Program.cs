4babf94b6c0e267d98478dfaa09f7deaa334d92c
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-08-12 14:19:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoAtual, anoNascimento, idade;
+
+            Console.WriteLine("Digite o ano atual");
+            anoAtual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o seu ano de nascimento");
+            anoNascimento = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anoNascimento;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você é maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade");
+            }
+            
+        }
+    }
+}
